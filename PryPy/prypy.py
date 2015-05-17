09d0f27962c3cd8ef193a6d5d5d56cc23bef69d1
Charlie Slater <charlieslater@gmail.com>
2015-05-17 10:34:51 -0700

    Added README.  Fixed always-use-dictionary bug.

--- PryPy/prypy.py
+++ PryPy/prypy.py
@@ -2,4 +2,4 @@ def ls(name,cnt=18):
     """print the last cnt attributes of an object"""
     from pprint import pprint
     from inspect import getmembers
-    pprint([x[0] for x in getmembers({})[-cnt:]])
+    pprint([x[0] for x in getmembers(name)[-cnt:]])
